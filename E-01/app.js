befe0dd853eaac2136c157388ce1e5cb0d0237ab
Juan Manuel Herrera Carrera <tampiko@gmail.com>
2018-09-12 16:44:07 -0500

    Apenas la version 0.0.1

---
+++ E-01/app.js
@@ -0,0 +1,15 @@
+var numero1 = 5;
+var numero2 = 8;
+
+if(...) {
+	console.log("numero1 no es mayor que numero2");
+}
+if(...) {
+	console.log("numero2 es positivo");
+}
+if(...) {
+	console.log("numero1 es negativo o distinto de cero");
+}
+if(...) {
+	console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
